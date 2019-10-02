89bd258d982c1b54aac6414ca6e8872f6db8a850
EmiKusaba <eeemi615@gmail.com>
2019-10-01 20:20:52 -0500

    added all of code

---
+++ src/animate-tut/animation.js
@@ -0,0 +1,7 @@
+function openNav(){
+  //thisi is where you dostuff
+  //show me nav-item--mobile
+let navMobile = document.querySelector('.nav-items--mobile')
+ navMobile.classList.toggle('showNav')
+  console.log('navMobile:',navMobile)
+}
