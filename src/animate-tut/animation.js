4a8e6f523424f8c2aa52837e7b81f9b830a78d08
EmiKusaba <eeemi615@gmail.com>
2019-10-01 20:25:23 -0500

    added javascript

--- src/animate-tut/animation.js
+++ src/animate-tut/animation.js
@@ -1,7 +1,7 @@
-function openNav(){
-  //thisi is where you dostuff
-  //show me nav-item--mobile
-let navMobile = document.querySelector('.nav-items--mobile')
- navMobile.classList.toggle('showNav')
-  console.log('navMobile:',navMobile)
+function openNav() {
+  // this is where you do stuff
+  // show me nav-item--mobile
+  let navMobile = document.querySelector('.nav-items--mobile')
+  navMobile.classList.toggle('showNav')
+  // console.log('navMobile:', navMobile.classList)
 }
